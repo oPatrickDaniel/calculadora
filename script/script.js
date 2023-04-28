61a217d65fd6b4a69b7747bd17ee3c8c4a2b3441
Patrick Daniel <contato.pdani@gmail.com>
2023-04-28 11:32:07 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,76 @@
+
+
+const display_result = document.getElementById("display_result")
+const display_preview = document.getElementById("display_preview")
+
+
+function inner_digit(num) {
+
+    let display_result = document.getElementById("display_result")
+
+    if (display_result.innerHTML == 0) {
+
+        display_result.innerText = num.value
+
+    } else {
+
+        display_result.innerText += num.value
+    }
+}
+
+function inner_preview(operator) {
+
+    if (display_preview.innerHTML == 0 && operator != ",") {
+
+        display_preview.innerText = display_result.textContent + operator.value
+
+    } else {
+
+        display_preview.innerText += display_result.textContent + operator.value
+    }
+
+    display_result.innerText = "0"
+}
+
+function to_clean() {
+
+    display_preview.innerText = "0"
+    display_result.innerText = "0"
+}
+
+function del() {
+    let display_result = document.getElementById("display_result").innerHTML
+    if (display_result.length < 2) {
+
+        document.getElementById("display_result").innerHTML = 0
+    } else {
+        document.getElementById("display_result").innerHTML = display_result.substring(0, display_result.length - 1)
+    }
+}
+
+function final_result() {
+
+    result = display_preview.innerHTML + display_result.innerHTML
+    display_preview.innerText = "0"
+    display_result.innerText = eval(result)
+}
+
+function sqrt() {
+    display_result.innerText = Math.sqrt(display_result.innerHTML)
+}
+
+function pow() {
+    display_result.innerText = Math.pow(display_result.innerHTML, 2)
+}
+
+function fracture() {
+    display_result.innerText = display_result.innerHTML / 100
+}
+
+function percentage() {
+    let base_valor = display_preview.innerHTML
+    let percentage = display_result.innerHTML
+
+    let result = base_valor.substring(0, base_valor.length - 1) * (percentage / 100)
+    display_result.innerText = result
+}
